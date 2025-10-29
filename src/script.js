62926da9cc99c6c7147ce17f5f33b64f2fd86b47
fierysleo <jolexandersan@gmail.com>
2025-10-30 00:19:05 +0800

    error code (npm e6s module required)

--- src/script.js
+++ src/script.js
@@ -1,2 +1,15 @@
 console.log("Script loaded successfully.");
+import { test() } from './addTaskForm.js';
 
+addtaskButton = document.getElementById("addTask");
+displaytaskButton = document.getElementById("displayTask");
+taskContainer = document.getElementById("taskContainer");
+
+addtaskButton.addEventListener("click", function() {
+    test;
+});
+
+displaytaskButton.addEventListener("click", function() {
+    alert("Displaying all tasks!");
+    // Here you can add logic to display tasks from a list or database
+});
